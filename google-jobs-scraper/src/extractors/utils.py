1290fe7e8fed34e1199a9f67341550645d9c99b7
mattheewBrown <matthewbrown8419@iejesusmirey.com>
2025-11-11 14:58:51 -0700

    feat: add google-jobs-scraper/src/extractors/utils.py

---
+++ google-jobs-scraper/src/extractors/utils.py
@@ -0,0 +1,9 @@
+thonimport json
+
+def read_json(file_path):
+    with open(file_path, 'r') as f:
+        return json.load(f)
+
+def write_json(data, file_path):
+    with open(file_path, 'w') as f:
+        json.dump(data, f, indent=4)
